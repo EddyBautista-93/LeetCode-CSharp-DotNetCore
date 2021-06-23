ff697133ade3d0451960fe694647d16cbd0ab705
EddyBautista <bautista.j.eddy@gmail.com>
2021-06-23 06:05:07 -0500

    Question Set Up

--- Program.cs
+++ Program.cs
@@ -4,23 +4,33 @@ namespace LeetCode_CSharp_DotNetCore
 {
     class Program
     {
-        //6.21 Palindrome LeetCode
+        //6.23 LeetCode
+        // Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
-        //Given an integer x, return true if x is palindrome integer.
+        // Symbol       Value
+        // I             1
+        // V             5
+        // X             10
+        // L             50
+        // C             100
+        // D             500
+        // M             1000
+        // For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
-        //An integer is a palindrome when it reads the same backward as forward.For example, 121 is palindrome while 123 is not.
+        // Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
-        public static bool IsPalindrome(int a)
+        // I can be placed before V (5) and X (10) to make 4 and 9. 
+        // X can be placed before L (50) and C (100) to make 40 and 90. 
+        // C can be placed before D (500) and M (1000) to make 400 and 900.
+        // Given a roman numeral, convert it to an integer.
+        public int RomanToInt(string s)
         {
-            string inputNumber = a.ToString();
-            char[] stringArray = inputNumber.ToCharArray();
-            Array.Reverse(stringArray);
-            string reverseInputNumber = new string(stringArray);
-            return inputNumber == reverseInputNumber ? true : false;
+
+            return 0;
         }
         static void Main(string[] args)
         {
-            IsPalindrome(321);
+            
         }
     }
 }
