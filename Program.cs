367d8b2332d14d0c7bb5166299436b7c6d69c1b8
EddyBautista <bautista.j.eddy@gmail.com>
2021-06-24 05:44:54 -0500

    TwoSum set up 6.24

--- Program.cs
+++ Program.cs
@@ -4,52 +4,30 @@ namespace LeetCode_CSharp_DotNetCore
 {
     internal class Program
     {
-        //6.23 LeetCode
-        // Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+        // 6.24 TwoSum
+        // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-        // Symbol       Value
-        // I             1
-        // V             5
-        // X             10
-        // L             50
-        // C             100
-        // D             500
-        // M             1000
-        // For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+        // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-        // Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+        // You can return the answer in any order.
 
-        // I can be placed before V (5) and X (10) to make 4 and 9.
-        // X can be placed before L (50) and C (100) to make 40 and 90.
-        // C can be placed before D (500) and M (1000) to make 400 and 900.
-        // Given a roman numeral, convert it to an integer.
+        // Example 1:
 
-        private static Dictionary<char, int> RomanMap = new Dictionary<char, int>()
+        // Input: nums = [2,7,11,15], target = 9
+        // Output: [0,1]
+        // Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+        // Example 2:
+
+        // Input: nums = [3,2,4], target = 6
+        // Output: [1,2]
+        // Example 3:
+
+        // Input: nums = [3,3], target = 6
+        // Output: [0,1]
+
+        public int[] TwoSum(int[] nums, int target)
         {
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-        };
-
-        public int RomanToInt(string s)
-        {
-            int result = 0;
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (i + 1 < s.Length && RomanMap[s[i]] < RomanMap[s[i + 1]])
-                {
-                    result -= RomanMap[s[i]];
-                }
-                else
-                {
-                    result += RomanMap[s[i]];
-                }
-            }
-            return result;
+            
         }
 
         private static void Main(string[] args)
