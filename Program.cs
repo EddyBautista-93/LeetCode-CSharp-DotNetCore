89c3ae6f737be2a37ea2152a52bbe14ab2201589
EddyBautista <bautista.j.eddy@gmail.com>
2021-06-28 20:06:14 -0500

    Valid Parentheses set up - leetcode

--- Program.cs
+++ Program.cs
@@ -4,30 +4,46 @@ namespace LeetCode_CSharp_DotNetCore
 {
     internal class Program
     {
-        // 6.24 TwoSum
-        // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        // 6.28 Valid Parentheses 
 
-        // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
-        // You can return the answer in any order.
+        // An input string is valid if:
+
+        // Open brackets must be closed by the same type of brackets.
+        // Open brackets must be closed in the correct order.
+
 
         // Example 1:
 
-        // Input: nums = [2,7,11,15], target = 9
-        // Output: [0,1]
-        // Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+        // Input: s = "()"
+        // Output: true
         // Example 2:
 
-        // Input: nums = [3,2,4], target = 6
-        // Output: [1,2]
+        // Input: s = "()[]{}"
+        // Output: true
         // Example 3:
 
-        // Input: nums = [3,3], target = 6
-        // Output: [0,1]
+        // Input: s = "(]"
+        // Output: false
+        // Example 4:
 
-        public int[] TwoSum(int[] nums, int target)
+        // Input: s = "([)]"
+        // Output: false
+        // Example 5:
+
+        // Input: s = "{[]}"
+        // Output: true
+
+
+        // Constraints:
+
+        // 1 <= s.length <= 104
+        // s consists of parentheses only '()[]{}'.
+
+        public bool IsValid(string s)
         {
-            
+
         }
 
         private static void Main(string[] args)
