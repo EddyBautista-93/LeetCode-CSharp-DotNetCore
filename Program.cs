63db0585c8996cfdef925715646cf17b7c13389d
EddyBautista <bautista.j.eddy@gmail.com>
2021-06-30 06:14:24 -0500

    set up

--- Program.cs
+++ Program.cs
@@ -4,79 +4,26 @@ namespace LeetCode_CSharp_DotNetCore
 {
     internal class Program
     {
-        // 6.28 Valid Parentheses
+        // 6.30 Highest and Lowest
+        // In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
-        // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+        // Example:
 
-        // An input string is valid if:
+        // Kata.HighAndLow("1 2 3 4 5");  // return "5 1"
+        // Kata.HighAndLow("1 2 -3 4 5"); // return "5 -3"
+        // Kata.HighAndLow("1 9 3 4 -5"); // return "9 -5"
 
-        // Open brackets must be closed by the same type of brackets.
-        // Open brackets must be closed in the correct order.
 
-        // Example 1:
 
-        // Input: s = "()"
-        // Output: true
-        // Example 2:
-
-        // Input: s = "()[]{}"
-        // Output: true
-        // Example 3:
-
-        // Input: s = "(]"
-        // Output: false
-        // Example 4:
-
-        // Input: s = "([)]"
-        // Output: false
-        // Example 5:
-
-        // Input: s = "{[]}"
-        // Output: true
-
-        // Constraints:
-
-        // 1 <= s.length <= 104
-        // s consists of parentheses only '()[]{}'.
-
-        public bool IsValid(string s)
-        {
-            Stack<char> endings = new Stack<char>(); // create a stack to hold reference
-
-            foreach (var x in s) x zx 
-            {
-                switch (x)
-                {
-                    case '(':
-                        endings.Push(')');
-                        break;
-
-                    case '[':
-                        endings.Push(']');
-                        break;
-
-                    case '{':
-                        endings.Push('}');
-                        break;
-
-                    case ')':
-                    case ']':
-                    case '}':
-                        if (endings.Count == 0 || endings.Pop() != x)
-                            return false;
-                        break;
-                }
-            }
-            return endings.Count == 0;
-        }
+  public static string HighAndLow(string numbers)
+  {
+    // Code here or
+    return "throw towel";
+  } 
 
         private static void Main(string[] args)
         {
-        isValid("()") // true
-        isValid("()[]{}") // true
-        isValid("(]") // false 
-        isValid("([)]") // false 
-        isValid("{[]}") // true
+            
         }
     }
 }
